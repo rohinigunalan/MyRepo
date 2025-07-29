aea08e30a6997f454338afbab5e2c648885bc09b
rohinigunalan <167920211+rohinigunalan@users.noreply.github.com>
2025-07-28 23:36:36 -0400

    confirm

--- New folder/Untitled-3.py
+++ New folder/Untitled-3.py
@@ -1,75 +1,53 @@
 {
-    "studentInfoManagerRequest": {
-        "requestName": "createPersonInfo",
-        "channel": {
-            "type": "C",
-            //"aiCd":"343705",
-            //"aiSourceCd":"CC"
-            "orgId" :"0"
+    "header": {
+        "version": "1.0.0",
+        "event": {
+            "category": "compliance",
+            "name": "data-subject-rights-information-notification",
+            "type": "data",
+            "version": "1.1.0",
+            "origin": "cb",
+            "timestamp": "2025-07-14T21:12:57.289Z"
         },
-        "personInfo": {
-            "matchInd": "Y",
-            "genderCd": "M",
-            "firstNm": "Ethan",
-            "lastNm": "Mitchell",
-            "genderTxt":"",
-            "middleInitial": "J",
-            "studentSearchOptInInd": "N",
-            "birthDt": "2004-11-08",
-            "hsGradDt": "2022-05-28",
-            "incomingAdminDt": null,
-            "aiCd": "654987",
-            "educationLevelCd": null,
-            "preferredFirstNm": "",
-            "sundayTestTakerInd": "N",
-            "deceasedInd": "N",
-            "activeInd": "Y"
-        },
-        "homeAddress": {
-            "streetAddr1": "1625 Cherry Creek Drive",
-            "streetAddr2": "Apt 3C",
-            "streetAddr3": "",
-            "urbanization": "",
-            "city": "Denver",
-            "zip5": "80209",
-            "zip4": "3456",
-            "stateCd": "CO",
-            "carrierRt": "",
-            "regionFipsCd": "",
-            "province": "",
-            "intlPostalCode": "",
-            "countyFipsCd": "",
-            "countryIsoCd": "US"
-        },
-        "personalEmail": {
-            "emailAddress": "ethan.mitchell@mailinator.com"
-        },
-        "homePhone": {
-            "ndc": "303",
-            "cc": "1",
-            "local": "5558901",
-            "prefInd": "Y",
-            "intlPhone": null,
-            "txtMsgAllowInd": "Y",
-            "deviceTypeCd": 1
-        },
-        "altPhone": {
-            "ndc": null,
-            "cc": "91",
-            "local": null,
-            "prefInd": "U",
-            "intlPhone": "9876543210",
-            "txtMsgAllowInd": "Y",
-            "deviceTypeCd": 2
-        },
-        "personParent": {
-            "firstNm": "David",
-            "lastNm": "Mitchell",
-            "emailAddr": "david.mitchell@mailinator.com",
-            "newsletterSubscriptionInd": "Y",
-            "ccSelectedInd": "Y"
-        },
-        "callerAppId": 303,
-        "aiDomainCd":"SAT"
+        "cb": {
+            "env": "qa"
+        }
+    },
+    "body": {
+        "requestId": "palm888",
+        "detail": {
+            "channels": [
+                "institutional"
+            ],
+            "scopes": [
+                "parent"
+            ],
+            "requestDetails": {
+                "formInput": {
+                    "requester": {
+                        "parentFirstName": "",
+                        "parentLastName": "",
+                        "parentEmail": ""
+                    },
+                    "subject": {
+                        "firstName": "firstName",
+                        "lastName": "lastName",
+                        "emailAddress": "notifyIntTest@cb.com",
+                        "alternateEmail": [
+                            "intTestAlt@cb.com"
+                        ],
+                        "residentialAddress": {},
+                        "birthdate": "09/09/1970",
+                        "phoneNumber": "800-867-5309",
+                        "studentGraduationYear": "",
+                        "educatorSchoolAffiliation": ""
+                    }
+                },
+                "enrichment": {
+                    "students": [],
+                    "professionals": []
+                }
+            }
+        }
     }
 }
---
+++ New folder/Untitled-4.py
@@ -0,0 +1,54 @@
+"enrichment": {
+          "students": {
+            "140131335": {
+              "personId": "140131335",
+              "catapultId": "us-east-1:ce099318-ce52-4352-a782-726e3d6ac860"
+            },
+            "140131337": {
+              "personId": "140131337",
+              "catapultId": "us-east-1:ce099318-ce52-4352-a782-726e3d6ac861"
+            },
+            "150231335": {
+              "personId": "150231335",
+              "catapultId": "us-east-1:ce099318-ce52-4352-a782-726e3d6ac862"
+            }
+          },
+          "professionals": {
+            "12432": {
+              "professionalId": "12432",
+              "professionalAccountId": "78712434",
+              "username": "joesmithatcenterville"
+            },
+            "12434": {
+              "professionalId": "12432",
+              "professionalAccountId": "78712434",
+              "username": "joesmithatcenterville"
+            }
+            
+            
+            
+##############################
+
+"enrichment": {
+                    "students": [
+                        {
+                            "catapultId": "us-east-1:8e07129e-af56-c221-bb4b-a0bfcacd8ffb",
+                            "personId": "200758264"
+                        },
+                        {
+                            "catapultId": "us-east-1:8e07129e-af2f-cc96-eb8a-c6cada9676f6",
+                            "personId": "200062692"
+                        }
+                    ],
+                    "professionals": [
+                        {
+                            "professionalAccountId": "78712",
+                            "professionalId": "54678",
+                            "username": "joesmithatcenterville"
+                        },
+                        {
+                            "professionalAccountId": "546789",
+                            "professionalId": "11111",
+                            "username": "joesmithatcenterville"
+                        }
+                    ]            
